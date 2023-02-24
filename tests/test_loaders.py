88077b18dfc05d21c89083417defee4a353852ac
johentsch <johannes.hentschel@epfl.ch>
2023-02-24 18:10:25 +0100

    factors out testing of ConfiguredDataframes default methods (in preparation for Features)

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,13 +1,41 @@
 from dataclasses import asdict
+from enum import Enum
+from typing import Type, Union
 
 import pytest
 from dimcat.data.facet import get_facet_class
 from dimcat.data.loader import DcmlLoader
+from dimcat.dtypes.base import ConfiguredDataframe
 
 
 @pytest.fixture()
 def loader(small_corpora_path):
     return DcmlLoader(directory=small_corpora_path)
+
+
+def default_methods_test(
+    configured_dataframe: ConfiguredDataframe,
+    supposed_name: Union[str, Enum],
+    constructor: Type[ConfiguredDataframe],
+):
+    assert isinstance(configured_dataframe, constructor)
+    assert configured_dataframe.dtype is supposed_name
+    assert configured_dataframe.name == supposed_name
+    facet_id = configured_dataframe.identifier
+    facet_df = configured_dataframe.df
+    facet_config = configured_dataframe.config
+    facet1 = constructor.from_config(
+        df=facet_df, config=facet_config, identifiers=facet_id
+    )
+    assert facet1 == configured_dataframe
+    facet2 = constructor.from_id(facet_id, df=facet_df)
+    assert facet2 == configured_dataframe
+    facet3 = constructor.from_default(df=facet_df, identifiers=facet_id)
+    assert facet3 == configured_dataframe
+    facet4 = constructor.from_df(df=facet_df, identifiers=facet_id)
+    assert facet4 == configured_dataframe
+    facet5 = constructor(**asdict(configured_dataframe))
+    assert facet5 == configured_dataframe
 
 
 def test_loader(loader):
@@ -17,21 +45,4 @@ def test_loader(loader):
             print(f"Testing {facet_name} returned by {piece.piece_id}:")
             facet_class = get_facet_class(facet_name)
             extracted_facet_object = piece.get_facet(facet_name)
-            assert isinstance(extracted_facet_object, facet_class)
-            assert extracted_facet_object.dtype is facet_name
-            assert extracted_facet_object.name == facet_name
-            facet_id = extracted_facet_object.identifier
-            facet_df = extracted_facet_object.df
-            facet_config = extracted_facet_object.config
-            facet1 = facet_class.from_config(
-                df=facet_df, config=facet_config, identifiers=facet_id
-            )
-            assert facet1 == extracted_facet_object
-            facet2 = facet_class.from_id(facet_id, df=facet_df)
-            assert facet2 == extracted_facet_object
-            facet3 = facet_class.from_default(df=facet_df, identifiers=facet_id)
-            assert facet3 == extracted_facet_object
-            facet4 = facet_class.from_df(df=facet_df, identifiers=facet_id)
-            assert facet4 == extracted_facet_object
-            facet5 = facet_class(**asdict(extracted_facet_object))
-            assert facet5 == extracted_facet_object
+            default_methods_test(extracted_facet_object, facet_name, facet_class)
